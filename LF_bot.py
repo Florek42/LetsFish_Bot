930f975ace2209cc8c23504fadb42162058d9bcc
Krzysztof F <krzysztoff0423@gmail.com>
2024-01-18 22:11:38 +0100

    repairs
    sterting not working, middle works, end not working

--- LF_bot.py
+++ LF_bot.py
@@ -1,43 +1,53 @@
+import pyautogui
 import time
 import random
-import pyautogui
 
-def lfb():
-    while True:
-        # Sprawdź kolor piksela na pozycji (749, 692)
-        while pyautogui.pixel(749, 692) == (61, 99, 135):
-            # Jeśli kolor na (749, 692) to (61, 99, 135), przestań trzymać spację
-            pyautogui.keyUp('space')
-            time.sleep(random.uniform(0.3, 0.7))
+def przenies_i_kliknij():
+    pyautogui.moveTo(655, 891, duration=1)
+    pyautogui.click()
 
-            # Poczekaj, aż kolor (61, 99, 135) na (749, 692) zmieni się na inny
-            while pyautogui.pixel(749, 692) == (61, 99, 135):
-                time.sleep(0.1)
+def sprawdz_kolor_i_trzymaj_spacje():
+    kolor_piksela = pyautogui.pixel(655, 891)
+    dopuszczalne_kolory = [(215, 180, 138), (217, 184, 142)]
 
-            # Zacznij trzymać spację
-            pyautogui.keyDown('space')
+    if kolor_piksela in dopuszczalne_kolory:
+        czas_trzymania = random.uniform(0.1, 0.94)
+        pyautogui.keyDown('space')
+        
+        # Czekaj, aż pojawi się kolor (61, 99, 135) na x=758, y=704
+        while pyautogui.pixel(758, 704) != (61, 99, 135):
+            time.sleep(0.1)
+        
+        pyautogui.keyUp('space')
 
-        # Przesuń kursor myszy do pozycji "zarzuc_wedke"
-        pyautogui.moveTo(655, 891, duration=1)
+def trzymaj_i_pusc_spacje():
+    while pyautogui.pixel(576, 673) != (255, 255, 255):
+        time.sleep(0.1)
 
-        # Kliknij lewym przyciskiem myszy
+    while pyautogui.pixel(749, 693) != (255, 255, 255):
+        pyautogui.keyDown('space')
+        time.sleep(0.1)
+
+    pyautogui.keyUp('space')
+
+def przenies_i_kliknij_po_kolorze():
+    pyautogui.keyUp('space')
+    time.sleep(1)
+    pyautogui.moveTo(929, 533, duration=1)
+    
+    kolor_piksela_5 = pyautogui.pixel(929, 533)
+    oczekiwany_kolor_5 = (228, 206, 182)
+
+    if kolor_piksela_5 == oczekiwany_kolor_5:
         pyautogui.click()
 
-        # Sprawdź kolor piksela na pozycji (655, 891)
-        while pyautogui.pixel(655, 891) == (215, 180, 138):
-            # Trzymaj spację przez losowy czas od 1.1 do 3.3 sekundy
-            pyautogui.keyDown('space')
-            time.sleep(random.uniform(1.1, 3.3))
+# Pierwszy etap
+przenies_i_kliknij()
+sprawdz_kolor_i_trzymaj_spacje()
 
-            # Poczekaj, aż kolor (215, 180, 138) na (655, 891) zniknie
-            while pyautogui.pixel(655, 891) == (215, 180, 138):
-                time.sleep(0.1)
+# Drugi etap
+trzymaj_i_pusc_spacje()
+trzymaj_i_pusc_spacje()
 
-            # Puść spację
-            pyautogui.keyUp('space')
-
-if __name__ == "__main__":
-    lfb()
-
-
-# gdy pojawia sie pasek lowienia ten zielono czerwony to za szybko ciagnie bot
+# Trzeci etap
+przenies_i_kliknij_po_kolorze()
