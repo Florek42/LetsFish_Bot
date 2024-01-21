1157f372b1c8e97e41f5858daf3f28fb65f62cd2
Krzysztof F <krzysztoff0423@gmail.com>
2024-01-21 14:29:01 +0100

    Update LF_bot.py

--- LF_bot.py
+++ LF_bot.py
@@ -1,53 +1,33 @@
 import pyautogui
-import time
 import random
+import time
 
-def przenies_i_kliknij():
-    pyautogui.moveTo(655, 891, duration=1)
-    pyautogui.click()
-
-def sprawdz_kolor_i_trzymaj_spacje():
-    kolor_piksela = pyautogui.pixel(655, 891)
-    dopuszczalne_kolory = [(215, 180, 138), (217, 184, 142)]
-
-    if kolor_piksela in dopuszczalne_kolory:
-        czas_trzymania = random.uniform(0.1, 0.94)
-        pyautogui.keyDown('space')
-        
-        # Czekaj, aż pojawi się kolor (61, 99, 135) na x=758, y=704
-        while pyautogui.pixel(758, 704) != (61, 99, 135):
-            time.sleep(0.1)
-        
-        pyautogui.keyUp('space')
-
-def trzymaj_i_pusc_spacje():
-    while pyautogui.pixel(576, 673) != (255, 255, 255):
-        time.sleep(0.1)
-
-    while pyautogui.pixel(749, 693) != (255, 255, 255):
-        pyautogui.keyDown('space')
-        time.sleep(0.1)
-
-    pyautogui.keyUp('space')
-
-def przenies_i_kliknij_po_kolorze():
-    pyautogui.keyUp('space')
-    time.sleep(1)
-    pyautogui.moveTo(929, 533, duration=1)
-    
-    kolor_piksela_5 = pyautogui.pixel(929, 533)
-    oczekiwany_kolor_5 = (228, 206, 182)
-
-    if kolor_piksela_5 == oczekiwany_kolor_5:
+def main():
+    try:
+        # Przeniesienie kursora na x=655, y=891 i kliknięcie lewego przycisku myszy
+        pyautogui.moveTo(655, 891, duration=1)
         pyautogui.click()
 
-# Pierwszy etap
-przenies_i_kliknij()
-sprawdz_kolor_i_trzymaj_spacje()
+        while True:
+            # Trzymanie przycisku spacji losowo przez czas od 0.1 do 0.94
+            czas_trzymania_spacji = random.uniform(0.1, 0.94)
+            pyautogui.keyDown('space')
+            time.sleep(czas_trzymania_spacji)
+            pyautogui.keyUp('space')
 
-# Drugi etap
-trzymaj_i_pusc_spacje()
-trzymaj_i_pusc_spacje()
+            # Sprawdzenie koloru na pozycji x=758, y=704
+            kolor_piksela_spacji = pyautogui.pixel(758, 704)
+            if kolor_piksela_spacji == (61, 99, 135):
+                break
 
-# Trzeci etap
-przenies_i_kliknij_po_kolorze()
+            # Sprawdzenie koloru na pozycji x=575, y=674
+            kolor_piksela_bialy_lewa = pyautogui.pixel(575, 674)
+            biale_kolory = [(74, 182, 48), (48, 91, 45), (82, 191, 42, 255), (60, 179, 47, 255)]
+            if kolor_piksela_bialy_lewa in biale_kolory:
+                print("wykryto_bialy_lewa")
+
+    except KeyboardInterrupt:
+        print("\nProgram zakończony.")
+
+if __name__ == "__main__":
+    main()
